891000f13bf0b391dd055399070c2058bdb6ea3c
VellyD <viddvd3@gmail.com>
2023-05-18 19:42:29 +0300

    progressBar range update functionality.

--- script.js
+++ script.js
@@ -49,6 +49,14 @@ function updateProgress() {
   currentTime.textContent = `${displayTime(video.currentTime)}/`;
   duration.textContent = `${displayTime(video.duration)}`;
 }
+
+// Change the video duration
+
+function setProgress(e) {
+  const newTime = e.offsetX / progressRange.offsetWidth;
+  progressBar.style.width = `${newTime * 100}%`;
+  video.currentTime = newTime * video.duration;
+}
 // Volume Controls --------------------------- //
 
 // Change Playback Speed -------------------- //
@@ -60,3 +68,4 @@ playBtn.addEventListener("click", togglePlay);
 video.addEventListener("click", togglePlay);
 video.addEventListener("timeupdate", updateProgress);
 video.addEventListener("canplay", updateProgress);
+progressRange.addEventListener("click", setProgress);
