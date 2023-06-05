9bfe61979594eb322bbfb84062f657e7c68b0f8a
VellyD <viddvd3@gmail.com>
2023-06-05 08:31:02 +0300

    adding toggleMute functionality.

--- script.js
+++ script.js
@@ -59,6 +59,7 @@ function setProgress(e) {
 }
 
 // Volume Controls --------------------------- //
+let lastVolume = 1;
 
 function changeVolume(e) {
   let volume = e.offsetX / volumeRange.offsetWidth;
@@ -74,7 +75,6 @@ function changeVolume(e) {
 
   volumeBar.style.width = `${volume * 100}%`;
   video.volume = volume;
-  console.log(volume);
 
   // Change icon depending on Volume
   volumeIcon.className = "";
@@ -85,7 +85,28 @@ function changeVolume(e) {
   } else if (volume === 0) {
     volumeIcon.classList.add("fas", "fa-volume-off");
   }
+  lastVolume = volume;
 }
+
+// Mute/Unmute
+
+function toggleMute() {
+  volumeIcon.className = "";
+
+  if (video.volume) {
+    lastVolume = video.volume;
+    video.volume = 0;
+    volumeBar.style.width = 0;
+    volumeIcon.classList.add("fas", "fa-volume-mute");
+    volumeIcon.setAttribute("title", "Unmute");
+  } else {
+    video.volume = lastVolume;
+    volumeBar.style.width = `${lastVolume * 100}%`;
+    volumeIcon.classList.add("fas", "fa-volume-up");
+    volumeIcon.setAttribute("title", "Mute");
+  }
+}
+
 // Change Playback Speed -------------------- //
 
 // Fullscreen ------------------------------- //
@@ -97,3 +118,4 @@ video.addEventListener("timeupdate", updateProgress);
 video.addEventListener("canplay", updateProgress);
 progressRange.addEventListener("click", setProgress);
 volumeRange.addEventListener("click", changeVolume);
+volumeIcon.addEventListener("click", toggleMute);
