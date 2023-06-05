579595792723c12a31ae26ef4c9ef34439cd0727
VellyD <viddvd3@gmail.com>
2023-06-05 08:38:24 +0300

    adding playback speed

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const volumeBar = document.querySelector(".volume-bar");
 const currentTime = document.querySelector(".time-elapsed");
 const duration = document.querySelector(".time-duration");
 const fullScreenBtn = document.querySelector(".fullscreen");
+const speed = document.querySelector(".player-speed");
 
 // Play & Pause ----------------------------------- //
 function showPlayIcon() {
@@ -108,7 +109,9 @@ function toggleMute() {
 }
 
 // Change Playback Speed -------------------- //
-
+function changeSpeed() {
+  video.playbackRate = speed.value;
+}
 // Fullscreen ------------------------------- //
 
 // EventListeners
@@ -119,3 +122,4 @@ video.addEventListener("canplay", updateProgress);
 progressRange.addEventListener("click", setProgress);
 volumeRange.addEventListener("click", changeVolume);
 volumeIcon.addEventListener("click", toggleMute);
+speed.addEventListener("change", changeSpeed);
